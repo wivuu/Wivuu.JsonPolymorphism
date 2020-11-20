e0fca2b770047516cfc825eaa3a07c9576ea06a4
Erik OLeary <erik.m.oleary@gmail.com>
2020-11-20 15:39:54 -0600

    Added more tests w/ diff json configuration options

--- Tests/SerializationTests.cs
+++ Tests/SerializationTests.cs
@@ -21,15 +21,63 @@ namespace Tests
     public class SerializationTests
     {
         [Fact]
-        public void TestEnumSerialization()
+        public void TestEnumCamelCaseSerialization()
         {
             JsonSerializerOptions options = new()
             {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-                IgnoreNullValues     = true
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
             };
 
             options.Converters.Add(new JsonStringEnumConverter());
+            
+            Animal[] animals = 
+            {
+                new Insect(NumLegs: 8, NumEyes: 6),
+                new Mammal(NumNipples: 6),
+                new Reptile(ColdBlooded: false),
+            };
+
+            var serialized          = JsonSerializer.Serialize(animals, options);
+            var animalsDeserialized = JsonSerializer.Deserialize<Animal[]>(serialized, options);
+            
+            if (animalsDeserialized is null)
+                throw new System.Exception("Unable to deserialize");
+
+            Assert.Equal(animals.Length, animalsDeserialized.Length);
+
+            for (var i = 0; i < animals.Length; ++i)
+                Assert.Equal(animals[i], animalsDeserialized[i]);
+        }
+        
+        [Fact]
+        public void TestEnumPascalSerialization()
+        {
+            JsonSerializerOptions options = new();
+            options.Converters.Add(new JsonStringEnumConverter());
+            
+            Animal[] animals = 
+            {
+                new Insect(NumLegs: 8, NumEyes: 6),
+                new Mammal(NumNipples: 6),
+                new Reptile(ColdBlooded: false),
+            };
+
+            var serialized          = JsonSerializer.Serialize(animals, options);
+            var animalsDeserialized = JsonSerializer.Deserialize<Animal[]>(serialized, options);
+            
+            if (animalsDeserialized is null)
+                throw new System.Exception("Unable to deserialize");
+                
+            Assert.Equal(animals.Length, animalsDeserialized.Length);
+
+            for (var i = 0; i < animals.Length; ++i)
+                Assert.Equal(animals[i], animalsDeserialized[i]);
+        }
+        
+        [Fact]
+        public void TestIntSerialization()
+        {
+            JsonSerializerOptions options = new();
             
             Animal[] animals = 
             {
